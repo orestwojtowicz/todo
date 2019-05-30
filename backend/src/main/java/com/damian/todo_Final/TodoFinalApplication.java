89b0e3c6252d2f680f0ce467022a29ea7c006121
orestwojtowicz <orestwojtowicz94@gmail.com>
2019-05-30 09:33:00 +0200

    initial commit

---
+++ backend/src/main/java/com/damian/todo_Final/TodoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.damian.todo_Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoFinalApplication.class, args);
+	}
+
+}
